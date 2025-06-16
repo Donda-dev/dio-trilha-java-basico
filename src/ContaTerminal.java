c441ce3df0c7d5c044848480af868a9cb086655e
Donda-Dev <donda897@gmail.com>
2025-06-16 14:27:25 -0300

    sistema de basico funcionando

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+     Scanner scanner =  new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+     double saldo = 980.23;
+
+     System.out.println("Por favor, digite o número da Conta:");
+     int numero = scanner.nextInt();
+
+     scanner.nextLine();
+
+     System.out.println("Por favor, digite o número da Agência:");
+     String agencia = scanner.nextLine();
+
+     System.out.println("Por favor, digite o nome do Cliente:");
+     String nomeCliente = scanner.nextLine();
+
+     System.out.println("Seu saldo em Conta é R$" + saldo);
+     System.out.println("Digite o valor para depósito:");
+     double deposito = scanner.nextDouble();
+
+     saldo += deposito;
+
+     System.out.println(" Olá " + nomeCliente + ", obrigado por ser nosso Cliente, sua Agência é " + agencia + " Conta " + numero + " seu saldo atual é R$" + saldo + " já está disponivel para saque");
+
+     scanner.close();
     }
 }
